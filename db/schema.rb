c80ad1b7f991f54bb2f9f5692e7b7b5c486cfe0e
Your Name <minamoyori19@outlook.jp>
2020-02-06 17:02:36 +0900

    add column user_id to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_06_063258) do
+ActiveRecord::Schema.define(version: 2020_02_06_075240) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name", null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
