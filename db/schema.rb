7792a1ccdccc6ae086a1e25af5a9f3d409fd199f
Your Name <minamoyori19@outlook.jp>
2020-02-06 12:43:44 +0900

    add task validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_04_091635) do
+ActiveRecord::Schema.define(version: 2020_02_06_024504) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
